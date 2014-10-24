ecad10ea356a503d156f3e9847c1ee1e04c0fd20
Stanislav Vlasic <svlasic@gmail.com>
2014-10-24 23:27:21 +0200

    Add hevc_para to vdec_profile_t in amplayer

--- amplayer/player/player_profile.h
+++ amplayer/player/player_profile.h
@@ -10,6 +10,11 @@ typedef struct {
 } sys_h264_profile_t;
 
 typedef struct {
+    int exist;
+    int support4k;
+} sys_hevc_profile_t;
+
+typedef struct {
     int progressive_enable;
     int interlace_enable;
     int wmv1_enable;
@@ -44,6 +49,7 @@ typedef struct {
 
 typedef struct _system_para_ {
     sys_h264_profile_t      h264_para;
+    sys_hevc_profile_t      hevc_para;
     sys_vc1_profile_t       vc1_para;
     sys_real_profile_t      real_para;
     sys_mpeg12_profile_t    mpeg12_para;
